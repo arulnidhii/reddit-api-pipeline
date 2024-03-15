021dc5374f2cbd55bf1b5dc8dd4d2646f0ef3287
Arulnidhi Karunanidhi <81353171+arulnidhii@users.noreply.github.com>
2024-03-15 18:53:09 +0000

    Add files via upload

---
+++ reddit_extraction_dag.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.providers.microsoft.azure.operators.azure_container_instances import AzureContainerInstancesOperator
+
+
+with DAG(
+    dag_id="reddit_data_extraction",
+    start_date=datetime(2024, 3, 14),
+    schedule_interval="@daily",
+    tags=["reddit", "data_extraction"],
+) as dag:
+
+    extract_data = AzureContainerInstancesOperator(
+        task_id="extract_reddit_data",
+        image_uri="redditapi.azurecr.io/reddit_extractor:latest",  # Replace with your ACR details
+        resource_group="Reddit-api-arul",  # Replace with your resource group
+        region="UK South",                 # Replace with your region
+    )
